70b5f0f1002ca50a4ed390593440affdbd546faa
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-09-24 20:06:28 +0200

    clean codes

--- backend/src/main/kotlin/backend/Utils.kt
+++ backend/src/main/kotlin/backend/Utils.kt
@@ -3,37 +3,15 @@
 package backend
 
 import backend.Log.log
-import org.apache.commons.lang3.RandomStringUtils
 import org.springframework.data.domain.Page
 import org.springframework.http.HttpHeaders
 import org.springframework.web.util.UriComponentsBuilder
 import java.io.UnsupportedEncodingException
 import java.net.URLEncoder.encode
 import java.nio.charset.StandardCharsets.UTF_8
-import java.security.SecureRandom
 import java.text.MessageFormat
 
-/*=================================================================================*/
-object RandomUtils {
-    private const val DEF_COUNT = 20
-    private val SECURE_RANDOM: SecureRandom by lazy {
-        SecureRandom().apply { nextBytes(ByteArray(size = 64)) }
-    }
 
-    private val generateRandomAlphanumericString: String
-        get() = RandomStringUtils.random(
-            DEF_COUNT, 0, 0, true, true, null, SECURE_RANDOM
-        )
-
-    val generatePassword: String
-        get() = generateRandomAlphanumericString
-
-    val generateActivationKey: String
-        get() = generateRandomAlphanumericString
-
-    val generateResetKey: String
-        get() = generateRandomAlphanumericString
-}
 /*=================================================================================*/
 
 /**
