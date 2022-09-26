6a803c30db5be64a5a95be6a5b282f790bf26de6
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-09-26 22:51:25 +0200

    rename Computer to OnBoardComputerApplication, refactor, simplify spring root Application class, reformat code, move bootstrap logique to Application.kt

--- backend/src/main/kotlin/backend/Application.kt
+++ backend/src/main/kotlin/backend/Application.kt
@@ -3,23 +3,136 @@
 package backend
 
 
+import backend.Constants.DEV_HOST
+import backend.Constants.SPRING_PROFILE_CLI
+import backend.Constants.SPRING_PROFILE_CLI_PROPS
+import backend.Constants.SPRING_PROFILE_CLOUD
+import backend.Constants.SPRING_PROFILE_CONF_DEFAULT_KEY
+import backend.Constants.SPRING_PROFILE_DEVELOPMENT
+import backend.Constants.STARTUP_LOG_MSG_KEY
+import backend.Log.log
+import org.springframework.beans.factory.getBean
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.runApplication
 import org.springframework.context.ApplicationContext
-import javax.annotation.PostConstruct
-
+import org.springframework.context.MessageSource
+import java.net.InetAddress.getLocalHost
+import java.net.UnknownHostException
+import java.util.*
+import kotlin.system.exitProcess
 
 /*=================================================================================*/
 @SpringBootApplication
 @EnableConfigurationProperties(ApplicationProperties::class)
-class Computer(
-    private val context: ApplicationContext
-) {
-    @PostConstruct
-    private fun init() = with(context) {
-        checkProfileLog(this)
-        //TODO: load computer config millennium-falcon.json
+class OnBoardComputerApplication
+
+
+/*=================================================================================*/
+object OnBoardComputerBootstrap {
+    @JvmStatic
+    fun main(args: Array<String>) = runApplication<OnBoardComputerApplication>(*args) {
+        with(this) {
+            setDefaultProperties(
+                hashMapOf<String, Any>(
+                    SPRING_PROFILE_CONF_DEFAULT_KEY to SPRING_PROFILE_DEVELOPMENT
+                )
+            )
+            setAdditionalProfiles(SPRING_PROFILE_DEVELOPMENT)
+        }
+    }.run { bootstrapLog(context = this) }
+
+}
+/*=================================================================================*/
+
+object OnBoardComputerCliBootstrap {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        log.info("STARTING : R2D2")
+        runApplication<OnBoardComputerApplication>(*args) {
+            setAdditionalProfiles(SPRING_PROFILE_CLI)
+            setDefaultProperties(SPRING_PROFILE_CLI_PROPS)
+        }
+        log.info("STOPPED  : R2D2")
+        exitProcess(0)
     }
 }
+/*=================================================================================*/
+
+fun checkProfileLog(context: ApplicationContext) = context.environment.activeProfiles.run {
+    when {
+        contains(SPRING_PROFILE_DEVELOPMENT) &&
+                contains(Constants.SPRING_PROFILE_PRODUCTION)
+        -> log.error(
+            context.getBean<MessageSource>().getMessage(
+                STARTUP_LOG_MSG_KEY,
+                arrayOf(
+                    SPRING_PROFILE_DEVELOPMENT,
+                    Constants.SPRING_PROFILE_PRODUCTION
+                ),
+                Locale.getDefault()
+            )
+        )
+    }
+    when {
+        contains(SPRING_PROFILE_DEVELOPMENT) &&
+                contains(SPRING_PROFILE_CLOUD)
+        -> log.error(
+            context.getBean<MessageSource>().getMessage(
+                STARTUP_LOG_MSG_KEY,
+                arrayOf(
+                    SPRING_PROFILE_DEVELOPMENT,
+                    SPRING_PROFILE_CLOUD
+                ),
+                Locale.getDefault()
+            )
+        )
+    }
+}
+
+
+/*=================================================================================*/
+
+private fun bootstrapLogMessage(
+    appName: String?,
+    protocol: String,
+    serverPort: String?,
+    contextPath: String,
+    hostAddress: String,
+    profiles: String
+): String = """${"\n\n\n"}
+----------------------------------------------------------
+go visit https://www.cheroliv.com    
+----------------------------------------------------------
+Application '${appName}' is running! Access URLs:
+Local:      $protocol://localhost:$serverPort$contextPath
+External:   $protocol://$hostAddress:$serverPort$contextPath
+Profile(s): $profiles
+----------------------------------------------------------
+${"\n\n\n"}""".trimIndent()
+
+
+/*=================================================================================*/
+
+private fun bootstrapLog(context: ApplicationContext): Unit =
+    log.info(
+        bootstrapLogMessage(
+            appName = context.environment.getProperty("spring.application.name"),
+            protocol = if (context.environment.getProperty("server.ssl.key-store") != null) "https"
+            else "http",
+            serverPort = context.environment.getProperty("server.port"),
+            contextPath = context.environment.getProperty("server.servlet.context-path") ?: "/",
+            hostAddress = try {
+                getLocalHost().hostAddress
+            } catch (e: UnknownHostException) {
+                log.warn(
+                    "The host name could not be determined, " +
+                            "using `localhost` as fallback"
+                )
+                DEV_HOST
+            },
+            profiles = context.environment.activeProfiles.joinToString(separator = ",")
+        )
+    )
 
 /*=================================================================================*/
