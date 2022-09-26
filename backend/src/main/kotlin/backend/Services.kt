90b167239e0f23c310d7bfaff194fe84f97cc82e
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-09-26 19:38:36 +0200

    move cli logique to Services.kt

--- backend/src/main/kotlin/backend/Services.kt
+++ backend/src/main/kotlin/backend/Services.kt
@@ -2,6 +2,12 @@
 
 package backend
 
+import backend.Log.log
+import org.springframework.boot.CommandLineRunner
+import org.springframework.context.ApplicationContext
+import org.springframework.context.ApplicationContextAware
+import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -14,3 +20,19 @@ import org.springframework.transaction.annotation.Transactional
 class RoadMapService(
   private val routeRepository: RouteRepository
 )
+
+@Component
+@Profile(Constants.SPRING_PROFILE_CLI)
+class OnBoardComputerCliRunner(
+  private var context: ApplicationContext,
+  private val roadMapService: RoadMapService
+) : CommandLineRunner, ApplicationContextAware {
+  override fun setApplicationContext(applicationContext: ApplicationContext) {
+    context = applicationContext
+  }
+  override fun run(vararg args: String?) {
+    //TODO: computation
+    log.info("bean provided by spring container : ${context.beanDefinitionNames.toList()}")
+  }
+
+}
