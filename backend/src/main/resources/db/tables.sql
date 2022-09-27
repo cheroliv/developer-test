c25a12d6f4173a87222198b9a0ef9659ee5627cc
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-09-27 04:13:07 +0200

    :backend: create db schema on tables.sql.

--- backend/src/main/resources/db/tables.sql
+++ backend/src/main/resources/db/tables.sql
@@ -1,1 +1,9 @@
 -- noinspection SqlNoDataSourceInspectionForFile
+CREATE TABLE IF NOT EXISTS `routes` (
+    `id`          UUID default random_uuid() PRIMARY KEY,
+    `origin`      VARCHAR,
+    `destination` VARCHAR,
+    `travel_time` INT
+);
+CREATE UNIQUE INDEX IF NOT EXISTS `uniq_idx_origin_destination`
+ON `routes` (`origin`, `destination`);
