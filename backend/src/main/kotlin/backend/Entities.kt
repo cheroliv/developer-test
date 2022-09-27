0bb4bf845368b7a4e132cfd9d94fb321e6d36282
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-09-27 04:14:52 +0200

    :backend: Entities.kt, map RouteEntity to fit table.sql schema.

--- backend/src/main/kotlin/backend/Entities.kt
+++ backend/src/main/kotlin/backend/Entities.kt
@@ -2,17 +2,25 @@
 
 package backend
 
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
 import java.util.*
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Positive
 
 
 /*=================================================================================*/
 interface RouteRecord
 
 /*=================================================================================*/
+@Table("`routes`")
 data class RouteEntity(
-    var id: UUID?,
+    @Id var id: UUID?,
+    @field:NotBlank
     val origin: String,
+    @field:NotBlank
     val destination: String,
+    @field:Positive
     val travelTime: Int,
 ) : RouteRecord
 /*=================================================================================*/
