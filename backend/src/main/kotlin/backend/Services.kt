fffcfafbbdaea3bb70bf7275a6ebbe00d82425c7
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-10-06 03:38:43 +0200

    :backend: update README.adoc, and starting dijkstra implementation.

--- backend/src/main/kotlin/backend/Services.kt
+++ backend/src/main/kotlin/backend/Services.kt
@@ -56,14 +56,6 @@ class RoadMapService(
             }
         }
 
-    //TODO: use sealed class
-    /**
-     * @param Pair<Route,Route>: a pair of from, to route
-     * @return Triple<String, Int, Int>?: a triple of location,autonomy & daysLeft
-     * could be null to express inability to make the travel and must refuel to the nearby planet or current planet.
-     */
-    fun hyperspaceJump(travel: Pair<Route, Route>): Triple<String, Int, Int>? = null
-
     @Transactional(readOnly = true)
     suspend fun giveMeTheOdds(strEmpire: String): Double {
         val empire = context.getBean<ObjectMapper>().readValue<Empire>(strEmpire)
@@ -86,4 +78,77 @@ class OnBoardComputerCliRunner(
     }
 
 }
+
+
+
 /*=================================================================================*/
+class Dijkstra(private val graph: AdjacencyList<Route>) {
+    private fun route(
+        destination: Vertex,
+        paths: HashMap<Vertex, Visit>
+    ): ArrayList<Edge> {
+        var vertex = destination
+        val path = arrayListOf<Edge>()
+        loop@ while (true) {
+            val visit = paths[vertex] ?: break
+            when (visit.type) {
+                VisitType.EDGE -> visit.edge?.let {
+                    path.add(it)
+                    vertex = it.source
+                }
+
+                VisitType.START -> break@loop
+            }
+        }
+        return path
+    }
+
+    private fun distance(
+        destination: Vertex,
+        paths: HashMap<Vertex, Visit>
+    ): Double {
+        val path = route(destination, paths)
+        return path.sumOf { it.weight ?: 0.0 }
+    }
+
+    fun shortestPath(start: Vertex): HashMap<Vertex, Visit> {
+        val paths: HashMap<Vertex, Visit> = HashMap()
+        paths[start]= Visit(VisitType.START)
+        val distanceComparator= Comparator<Vertex> { first, second ->
+            (distance(second, paths) - distance(first, paths)).toInt()
+        }
+        val priorityQueue=ComparatorPriorityQueueImpl(distanceComparator)
+        priorityQueue.enqueue(start)
+        return TODO("Provide the return value")
+    }
+
+    class ComparatorPriorityQueueImpl(distanceComparator: Comparator<Vertex>) {
+        //TODO: add empire constraint
+        fun enqueue(start: Vertex) {
+            TODO("Not yet implemented")
+        }
+
+    }
+}
+
+class AdjacencyList<T> {
+
+}
+
+class Visit(
+    val type: VisitType, val edge: Edge? =
+        null
+)
+
+class Edge(val source: Vertex, val weight: Double?) {
+
+}
+
+enum class VisitType {
+    START,
+    EDGE
+}
+
+class Vertex {
+
+}
