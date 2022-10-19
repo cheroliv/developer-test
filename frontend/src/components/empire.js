aa124a571f0b55a11b22f418203c458224bffac0
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-10-20 00:10:53 +0200

    :frontend: working on the file form.

--- frontend/src/components/empire.js
+++ frontend/src/components/empire.js
@@ -3,45 +3,53 @@ import axios from "axios";
 
 
 const api_url = "http://localhost:8080/api/give-me-the-odds";
-//const empire = "empire"
 
 export default function Empire() {
     const [uploadFile, setUploadFile] = React.useState();
 
     const submitForm = (event) => {
-//        event.preventDefault();
+        event.preventDefault();
 
+        const empire = `{
+            "countdown": 7,
+            "bounty_hunters": [
+              {
+                "planet": "Hoth",
+                "day": 6
+              },
+              {
+                "planet": "Hoth",
+                "day": 7
+              },
+              {
+                "planet": "Hoth",
+                "day": 8
+              }
+            ]
+          }`
 
-//        const empire = fs.readFileSync(uploadFile, {encoding: 'utf-8'})
-//        console.log(empire)
+        //        const empire = fs.readFileSync(uploadFile, {encoding: 'utf-8'})
+        console.log(empire)
 
-//        axios
-//            .post(api_url, empire)
-//            .then((response) => {
-////                 successfully uploaded response
-//                console.log(empire)
-//            })
-//            .catch((error) => {
-////                 error response
-//                console.log(empire)
-//            });
-//    };
+        axios
+            .post(api_url, empire)
+            .then((response) => {
+                //                 successfully uploaded response
+                console.log(empire)
+            })
+            .catch((error) => {
+                //                 error response
+                console.log(empire)
+            });
+    };
 
-    return ( <
-        div >
-        <
-        form onSubmit = { submitForm } >
-        <
-        input type = "file"
+    return (<div>
+        <form onSubmit = { submitForm } >
+        <input type = "file"
         name = "empire"
-        onChange = {
-            (e) => setUploadFile(e.target.files) }
-        /> <
-        br / >
-        <
-        input type = "submit" / >
-        <
-        /form> <
-        /div>
-    );
+        onChange = {(e) => setUploadFile(e.target.files)}/>
+        <br/>
+        <input type = "submit" / >
+        </form>
+        </div>);
 }
