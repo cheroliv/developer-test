30961ce6e00fb46a751a41e01e5637dbb6cf22e3
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-10-20 07:31:29 +0200

    :frontend: fixed display result.

--- frontend/src/components/empire.js
+++ frontend/src/components/empire.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 
 const api_url = "http://localhost:8080/api/give-me-the-odds";
 
-export default function Empire() {
-    const [uploadFile, setUploadFile] = React.useState();
 
-    const submitForm = (event) => {
+export default function Empire() {
+    const [uploadFile, setUploadFile] = useState();
+    const [odds,setOdds] = useState();
+
+    const submitEmpire = (event) => {
         event.preventDefault();
         const reader = new FileReader();
         reader.addEventListener('load', (event) => {
@@ -15,21 +17,26 @@ export default function Empire() {
                        reader.result,
                         { headers: {"Content-Type": "application/json"}})
                 .then((response) => {
-                    console.log(response.data);
-                    //TODO: display odds
+                        setOdds(response.data);
+                        console.log(odds);
                     })
-                .catch((error) => {console.log(error.response.data.message); });
+                .catch((error) => { console.log(error.response.data.message); });
         });
         reader.readAsText(uploadFile.item(0));
     };
 
     return (<div>
-        <form onSubmit = { submitForm } >
+        <form onSubmit = { submitEmpire } >
             <input type = "file"
                    name = "empire"
                    onChange = {(e) => setUploadFile(e.target.files)}/>
             <br/>
             <input type = "submit" / >
         </form>
+	<p>
+	{odds == 0 && <>probability of success: 0 %</>}
+	{odds == 1 && <>probability of success: 100 %</>}
+	{odds < 1 && odds >0 && <>probability of success: {odds} %</>}
+	</p>
     </div>);
 }
